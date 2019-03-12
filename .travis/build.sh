de8db29e8c9059608a03f67dfea7c70211faf027
hax0kartik <pokemonemerald0@gmail.com>
2019-03-12 09:04:27 +0000

    Since evrything is working, switch back to main branch

--- .travis/build.sh
+++ .travis/build.sh
@@ -26,6 +26,6 @@ chmod +x makerom
 cp -s $PWD/makerom /usr/bin/makerom
 
 # Finally clone and build luma3ds
-git clone -b ctrulib-fsreg-pxipm --single-branch https://github.com/AuroraWright/Luma3DS.git 
+git clone https://github.com/AuroraWright/Luma3DS.git 
 cd Luma3DS
 make firm -j
