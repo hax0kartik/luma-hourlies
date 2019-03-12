c77a78ecde8c0db0fecb0cdef81922cfde0429e5
hax0kartik <pokemonemerald0@gmail.com>
2019-03-12 05:55:09 +0000

    Fix some more issues

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,6 +7,7 @@ cd ctrulib/libctru
 make install release -j
 
 # Install armips, firmtool and makerom
+cd /build_dir
 curl -LJO https://ev1l0rd.s-ul.eu/mEIk4atQ
 apt-get install unzip
 unzip armips-5996abc.zip -d armips
@@ -15,10 +16,11 @@ chmod +x armips
 echo $PWD
 cp -s $PWD/armips /usr/bin/armips
 
-apt-get install python python-pip
+apt-get install python python-pip -y
 pip install cryptography
 pip install git+https://github.com/TuxSH/firmtool.git
 
+cd /build_dir
 curl -LJO https://github.com/profi200/Project_CTR/releases/download/0.15/makerom_015_ctrtool.zip
 unzip makerom_015_ctrtool -d makerom
 cd makerom/Linux_x86_64
@@ -28,6 +30,6 @@ cp -s $PWD/makerom /usr/bin/makerom
 # Finally clone and build luma3ds
 git clone https://github.com/AuroraWright/Luma3DS.git 
 cd Luma3DS
-make -j
+make firm -j
 
 mv -v ./out/* /build_dir/out/*
