8baeb45574f3d513d39844cdc57867cae53c8a2b
hax0kartik <pokemonemerald0@gmail.com>
2019-03-19 03:16:28 +0000

    armips is not needed anymore

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,14 +8,7 @@ unzip ctrulib-1.5.1.zip -d ctrulib
 cd ctrulib/ctrulib-1.5.1/libctru 
 make install release -j
 
-# Install armips, firmtool and makerom
-cd /build_dir
-curl -LJO https://ev1l0rd.s-ul.eu/mEIk4atQ
-unzip armips-5996abc.zip -d armips
-cd armips/armips-5996abc/x86_64
-chmod +x armips
-echo $PWD
-cp -s $PWD/armips /usr/bin/armips
+# Install firmtool and makerom
 
 apt-get update && apt-get install python3 python3-pip -y
 pip3 install cryptography
