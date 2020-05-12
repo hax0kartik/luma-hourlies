26c345d200478d26a1ec75216ff81e18fd9ba5f2
hax0kartik <pokemonemerald0@gmail.com>
2020-05-12 20:08:21 +0530

    Properly increement tags

---
+++ .files/get_build_number.py
@@ -0,0 +1,7 @@
+import requests, json, sys
+
+r = requests.get("https://api.github.com/repos/hax0kartik/Luma-hourlies/releases/latest")
+if(r.ok):
+    data = json.loads(r.text or r.content)
+    latest_tag_name = data['name'][:data['name'].find('-')]
+    print(latest_tag_name)
